72efda911ee42b47004d0b008f7fd0783d68bcb8
Mostafa Hassan <mostafahassan8799@gmail.com>
2023-05-12 17:21:15 +0200

    note item view

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: const [
+          Text(
+            'Notes',
+            style: TextStyle(
+              fontSize: 32,
+            ),
+          ),
+          Spacer(),
+          CustomSearchIcon(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/notes_view_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'custom_note_item.dart';
+
+class NotesViewList extends StatelessWidget {
+  const NotesViewList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context, index) {
+      return const Padding(
+        padding: EdgeInsets.symmetric(vertical: 4),
+        child: NoteItem(),
+      );
+    });
+  }
+}
