4c78cdd5d9fddd891c202a26d03f2209f80549b0
Mostafa Hassan <mostafahassan8799@gmail.com>
2023-05-13 20:54:40 +0200

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,49 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(247, 255, 201, 119),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 24),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const EditNoteView(),
+            ));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(247, 255, 201, 119),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 4, bottom: 8),
+                child: Text(
+                  'this my note app',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(.6), fontSize: 20),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  size: 30,
+                ),
+                color: Colors.black,
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 4, bottom: 8),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'this my note app',
-                style: TextStyle(
-                    color: Colors.black.withOpacity(.6), fontSize: 20),
+                'june26 , 2023',
+                style: TextStyle(color: Colors.black.withOpacity(.6)),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                size: 30,
-              ),
-              color: Colors.black,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'june26 , 2023',
-              style: TextStyle(color: Colors.black.withOpacity(.6)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
