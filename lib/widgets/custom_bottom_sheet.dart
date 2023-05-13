471303ca73d68e66cdd6ad5cd5741bc6a4696c82
Mostafa Hassan <mostafahassan8799@gmail.com>
2023-05-13 16:30:18 +0200

    add field to notes bottom sheet

--- lib/widgets/custom_bottom_sheet.dart
+++ lib/widgets/custom_bottom_sheet.dart
@@ -8,13 +8,22 @@ class CustomBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: const [
           SizedBox(
             height: 32,
           ),
-          CustomTextField(),
+          CustomTextField(
+            hint: 'title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'content',
+            maxLines: 5,
+          )
         ],
       ),
     );
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 import '../constants/string_const.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  final String hint;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: kPrimaryColor,
+      maxLines: maxLines,
       decoration: InputDecoration(
-          hintText: 'Title',
+          hintText: hint,
           enabledBorder: buildBorder(),
           focusedBorder: buildBorder(color: kPrimaryColor)),
     );
